d0e6cd168474712a938fb332d465a72754a4e45f
Momongako <kuuhaku771998@gmail.com>
2022-07-16 23:59:21 +0700

    asd

--- BTVN05/Ex01/WithRead.java
+++ BTVN05/Ex01/WithRead.java
@@ -5,16 +5,73 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class WithRead {
-    public static void main(String[] args) throws IOException {
 
-        FileReader fr = new FileReader("file-content.txt");
-        BufferedReader br = new BufferedReader(fr);
+    void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
 
-        int i;
-        while ((i = br.read()) != -1) {
-            System.out.print((char) i);
+            // Di chuyển các phần tử của arr [0 ... i - 1], lớn hơn key
+            // đến một vị trí trước vị trí hiện tại của chúng
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
         }
-        br.close();
-        fr.close();
+    }
+
+    // In các phần tử của mảng
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = null;
+        int line;
+        char[] array = new char[100];
+        int intArray = 0;
+        int count = 0;
+        try {
+            br = new BufferedReader(new FileReader("BTVN05\\Ex01\\file-content.txt"));
+
+            // Đọc các ký tự và lưu vào array
+
+            br.read(array);
+            System.out.print("Array : ");
+            System.out.println(array);
+            int result = 0;
+
+            // intArray = charArrayToInteger(array);
+            // System.out.println(intArray);
+            // intArray = charArrayToInt(array);
+            // System.out.println(intArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int charArrayToInteger(char[] array) {
+        String arr = new String(array);
+        int number = Integer.parseInt(arr);
+
+        return number;
+    }
+
+    private static int charArrayToInt(char[] array) {
+        int result = 0;
+        int length = array.length - 1;
+
+        for (int i = 0; i <= length; i++) {
+            int digit = array[i] - '0'; // we don't want to cast by using (int)
+            result *= 10;
+            result += digit;
+        }
+        return result;
     }
 }
