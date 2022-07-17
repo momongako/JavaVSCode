d0bd02855287295120b26e24ca24e60c6f92b841
momongako <kuuhaku771998@gmail.com>
2022-07-17 16:29:09 +0700

    ok

--- BTVN05/Ex01/WithRead.java
+++ BTVN05/Ex01/WithRead.java
@@ -11,35 +11,17 @@ public class WithRead {
 
    
 
-    // In các phần tử của mảng
+    
   
     public static void main(String[] args) throws IOException {
         BufferedReader br = null;
         
-        char[] array = new char[100];
-        int[] number = new int[array.length];
-        int[] number1 = new int[10];
+        
+        
         try {
             br = new BufferedReader(new FileReader("BTVN05\\Ex01\\file-content.txt"));
             DataOutputStream out = new DataOutputStream(new FileOutputStream("BTVN05\\Ex01\\Numbers.txt"));
-            // Đọc các ký tự và lưu vào array
-            // br.read(array);
-            // br.skip(5);
-
-            // System.out.print("array : ");
-            // System.out.println(array);
             
-
-            // int i;
-            // int count = 0;
-            // int res  = 0 ;
-            
-            // int a[] = new int[11];
-            // String line = br.readLine(); // to read multiple integers line
-            // String[] strs = line.trim().split("\\s+");
-            // for (int i = 0; i < 10; i++) {
-            //     a[i] = Integer.parseInt(strs[i]);
-            // }
             
 
             String[] integersInString = br.readLine().split(" ");
@@ -60,34 +42,17 @@ public class WithRead {
                     a[j + 1] = key;
                 }
                 for (int i : a) {
-                    System.out.print(i + " ");
-                    out.writeBytes(i + "  ,");
+                    // System.out.print(i + " ");
+                    out.writeBytes(i + " ");
                 }
-            // while ((i = br.read()) != -1) {
-            //      number[count] = Character.getNumericValue((char) i);
-            //     count++;
+                // Đọc file Number
+                
 
-                
-            //     System.out.print("check" +(char) i);
-                
-             
-            // }
-            
-            // for (int j : number) {
-            //    if (j != 0 && j >0) {
-                 
-            //     System.out.print(j + " ");
-                
-            //    }
-            // }
             br.close();
-        
+            
 
 
-            // intarray = chararrayToInteger(array);
-            // System.out.println(intarray);
-            // intarray = chararrayToInt(array);
-            // System.out.println(intarray);
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
